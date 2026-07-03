91c654cac065f1ed6bc9cbadb8a6322d272796e4
Vinícius Macêdo <vinimaiscedo08@gmail.com>
2026-07-03 15:47:23 -0300

    regra de negócio aqui

---
+++ contracts/libraries/Validacoes.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library Validacoes {
+
+    function validarNome(string memory nome) internal pure {
+        require(bytes(nome).length > 0, "Nome obrigatorio");
+    }
+
+    function validarCPF(string memory cpf) internal pure {
+        require(bytes(cpf).length > 0, "CPF obrigatorio");
+    }
+
+    function validarIdade(uint idade) internal pure {
+        require(idade > 12, "Idade deve ser maior que 12");
+    }
+
+}
