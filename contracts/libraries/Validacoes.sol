dde273e11af5f5ddff3396035bf1c5e804d4188f
Vinícius Macêdo <vinimaiscedo08@gmail.com>
2026-07-03 17:01:13 -0300

    mudando lógicad e validação de CPF para verificar que  os caracteres também são números

--- contracts/libraries/Validacoes.sol
+++ contracts/libraries/Validacoes.sol
@@ -8,7 +8,18 @@ library Validacoes {
     }
 
     function validarCPF(string memory cpf) internal pure {
-        require(bytes(cpf).length > 0, "CPF obrigatorio");
+
+        bytes memory cpfBytes = bytes(cpf);
+
+        require(cpfBytes.length == 11, "CPF deve possuir 11 digitos");
+
+        for (uint i = 0; i < cpfBytes.length; i++) {
+            require(
+                cpfBytes[i] >= bytes1("0") &&
+                cpfBytes[i] <= bytes1("9"),
+                "CPF deve conter apenas numeros"
+            );
+        }
     }
 
     function validarIdade(uint idade) internal pure {
